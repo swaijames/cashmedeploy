c59768ae825b9e8429b20c54532a5b1df7a36853
swayjames <swaijames255@gmail.com>
2024-08-09 11:59:43 +0300

    seventh

--- constant/index.jsx
+++ constant/index.jsx
@@ -151,7 +151,7 @@ export const teamMembers = [
   {
     name: 'Ishmael Oneya',
     role: 'CashMe Tanzania,Head of Credit Risk & Compliance',
-    image: '/assets/team/oneya.jpg',
+    image: '/assets/team/boss-gumo.jpg',
     social: {
 
       linkedin: 'https://linkedin.com/in/darrenrandolph',
@@ -161,7 +161,7 @@ export const teamMembers = [
   {
     name: 'Ombeni Uhuru ',
     role: 'CashMe Tanzania,Head of Trading & Investment',
-    image: '/assets/team/omben.jpg',
+    image: '/assets/team/boss-gumo.jpg',
     social: {
 
       linkedin: 'https://linkedin.com/in/darrenrandolph',
